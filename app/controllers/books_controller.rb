da8fd84cf9f9c0aa72ed0f57256024d441c33074
YuriNakano <yuriinlethbridge@outlook.com>
2022-07-28 08:21:41 +0000

    [Finish] book/Create機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,11 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+
   def index
   end
 
@@ -10,5 +15,10 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
