cb2851e3728ddb24ea5f96824e60d0cae7721bf6
YuriNakano <yuriinlethbridge@outlook.com>
2022-07-29 08:45:18 +0000

    [Modified] ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
   resources :books
   
   # get '/books' => 'books#index', as: 'books'
-  # post '/books' => 'books#create'
+  # post '/books' => 'books#create', as: 'books'
   # get '/books/new' => 'books#new', as: 'new_book'
   # get 'books/:id' => 'books#show', as: 'book'
   # get '/books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch '/books/:id' => 'books#update', as: 'update_book'
-  # put '/books/:id' => 'books#update'
-  # delete '/books/:id' => 'books#destroy'
+  # patch '/books/:id' => 'books#update', as: 'book'
+  # put '/books/:id' => 'books#update', as: 'book'
+  # delete '/books/:id' => 'books#destroy', as: 'book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
