103214de960d50ded6a7c1b63b76191202b2aa1b
YuriNakano <yuriinlethbridge@outlook.com>
2022-07-29 08:18:22 +0000

    [Modified] book/updateアクション/ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # get '/books/new' => 'books#new', as: 'new_book'
   # get 'books/:id' => 'books#show', as: 'book'
   # get '/books/:id/edit' => 'books#edit', as: 'edit_book'
-  # patch '/books/:id' => 'books#update'
+  patch '/books/:id' => 'books#update', as: 'update_book'
   # put '/books/:id' => 'books#update'
   # delete '/books/:id' => 'books#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
