c7fd080e32c7d767f1d7066138fba5a3e12b6f92
YuriNakano <yuriinlethbridge@outlook.com>
2022-07-29 09:50:45 +0000

    [Add] フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
+    flash[:notice] = "Book was successfully created."
     redirect_to book_path(book.id)
   end
 
@@ -24,6 +25,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
   end
   
