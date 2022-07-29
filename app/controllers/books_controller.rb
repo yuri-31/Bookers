4a6da488d4e5918fb3ecb0e3a1e0dbdf809df4de
YuriNakano <yuriinlethbridge@outlook.com>
2022-07-29 10:03:59 +0000

    [Start] バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,11 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    flash[:notice] = "Book was successfully created."
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else render :new
+    end
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+    
+    validates :title, presence: true
+    validated :body, presence: true
 end
